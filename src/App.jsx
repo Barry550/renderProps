c02835680339892d1af24d95e9342c77ab90a81d
Barry550 <mamadoubarry6059@gmail.com>
2022-07-21 02:32:01 +0000

    add to txt

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,10 @@ function App() {
     <div className="App">
     <h1 style={{fontSize: '40px'}}>Vegeta Vs Goku vs Broly</h1>
     <h3>medaille de bronze</h3>
-    <h2>tes trop fort</h2>
-<<<<<<< HEAD
-=======
-    <h3>medaille de bronze</h3>
-    <h3>medaille de bronze</h3>
-    <h3>medaille de bronze</h3>
-<<<<<<< HEAD
+    <h3>medaille d'argent</h3>
+
 
     <hr/> 
-=======
-<<<<<<< HEAD
-    <h3>medaille d'or</h3>
-=======
-    <h3>medaille d'argent</h3>
->>>>>>> 0fddc93fb8dbd3109e282e2322fd6d4d934680aa
->>>>>>> 8b30a9500b45d5162b1cd06402332fe6f38c839e
-    <hr/>
->>>>>>> 0615326fdfacb40c2cc0c7cff3304a92923c20cc
     <div className="container">
         <AddHits render={(state, addHits, name)=> (
           <>
